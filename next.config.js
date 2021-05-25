dba87b97e7446cb257627dd6dd7217598f378b39
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2021-05-25 11:36:46 -0300

    added next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   imagea: {
-    domains: ['http://packseartes.kaizenfotoprodutos.com.br'],
+    domains: [
+      'http://packseartes.kaizenfotoprodutos.com.br',
+      'packseartes.kaizenfotoprodutos.com.br',
+      'kaizenfotoprodutos.com.br',
+      'https://kaizenfotoprodutos.com.br',
+    ],
   },
 };
